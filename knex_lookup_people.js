5b82b689716d82722e8a3beb724bf4105ecfe12c
Sungil Saul Lee <jangsoo86@gmail.com>
2019-04-23 23:26:40 +0000

    migration

--- knex_lookup_people.js
+++ knex_lookup_people.js
@@ -21,7 +21,7 @@ knex.select('*').from('famous_people')
       console.log("Searching ...");
       console.log(`Found ${result.length} person(s) by the name '${personName}'`)
       for (i = 0; i < result.length; i++) {
-      console.log(`- ${i + 1}: ${result[i].first_name} ${result[i].last_name}, born '${result[i].birthdate}'`);
+        console.log(`- ${i + 1}: ${result[i].first_name} ${result[i].last_name}, born '${result[i].birthdate}'`);
       }
     })
     .finally(function() {
